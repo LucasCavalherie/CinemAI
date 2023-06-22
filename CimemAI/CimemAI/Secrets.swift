f8221058be8ab8718fbcfc4552e898af7330295a
Lucas Cavalherie <lucascavalherie@Lucas-Cavalherie-MacBook.local>
2023-06-22 15:05:54 -0300

    gitignore
    Signed-off-by: Lucas Cavalherie <lucascavalherie@Lucas-Cavalherie-MacBook.local>

---
+++ CimemAI/CimemAI/Secrets.swift
@@ -0,0 +1,8 @@
+//
+//  Secrets.swift
+//  CimemAI
+//
+//  Created by Lucas Cavalherie on 22/06/23.
+//
+
+import Foundation
---
+++ CimemAI/CimemAI/View/MethodScreen.swift
@@ -0,0 +1,20 @@
+//
+//  MethodScreen.swift
+//  CimemAI
+//
+//  Created by Lucas Cavalherie on 22/06/23.
+//
+
+import SwiftUI
+
+struct MethodScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MethodScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        MethodScreen()
+    }
+}
