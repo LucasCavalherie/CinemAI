3de658deeac6aeba6e61783c3089de522fc6082e
lucascavalherie <lucascavalherie01@gmail.com>
2023-06-29 15:23:00 -0300

    Fix conflits

--- CimemAI/CimemAI/Secrets.swift
+++ CimemAI/CimemAI/Secrets.swift
@@ -2,13 +2,13 @@
 //  Secrets.swift
 //  CimemAI
 //
-//  Created by Júlia Savaris on 27/06/23.
+//  Created by Lucas Cavalherie on 29/06/23.
 //
 
 import Foundation
 
 class Secrets {
     static let CHATGPT_API_KEY = "KEY"
-    static let IMDB_API_KEY = "KEY"
+    static let IMDB_API_KEY = "k_cj8fffs5"
     static let TMDB_API_KEY = "KEY"
 }
