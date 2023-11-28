2fdf8db25fa3eeed5fb73d7ed4430e083bca97cf
Andre <andre_fabriciow@hotmail.com>
2023-11-28 16:57:21 -0300

    Reorganizando o projeto

---
+++ CimemAI/Secrets.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+class Secrets {
+    static let CHATGPT_API_KEY = "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"
+    static let IMDB_API_KEY = "k_cj8fffs5---"
+    static let TMDB_API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyY2M3ZDViNjdlZDY3YTdlZGQ0YTQxMTMwZGJiODc4ZiIsInN1YiI6IjY0OTljMzMwYjM0NDA5MDExYzdjYWQ3NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HcGGZcuqXS7H_WsH18mWDY5O2j3X-EUw7ZVTpph_-qE"
+}
