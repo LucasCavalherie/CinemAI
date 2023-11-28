cf315dc3c851ed94f49f124abb1a9d9c35fce74c
André Wozniack <84557148+andrewozexe@users.noreply.github.com>
2023-11-28 19:36:45 -0300

    Delete CimemAI directory

--- CimemAI/FilmFinder/Model/ChatGptFilterModel.swift
+++
@@ -1,8 +0,0 @@
-
-import SwiftUI
-
-// Classe observável para armazenar o estado e os valores selecionados
-class ChatGptFilterModel: ObservableObject {
-    @Published var tipo: String = ""
-    @Published var filtros: [String] = [""]
-}
--- CimemAI/FilmFinder/Secrets.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Secrets.swift
-//  FilmFinder
-//
-//  Created by André Wozniack on 28/11/23.
-//
-
-import Foundation
